7b7d87f31e5800a89ca9f0c754577a87f4516548
paulenrique <ph_ab@hotmail.com>
2018-08-24 14:42:00 -0300

    Update ex1.c

--- Lista 1/ex1.c
+++ Lista 1/ex1.c
@@ -7,3 +7,26 @@ int main()
   printf("Resultado: %.0f", pow(b,n));
   return 0;
 }
+
+
+--------------------------------------
+  Paulo:
+
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+
+	int num, exp, elevado;
+	
+	printf("Informe o valor de b: ");
+	scanf("%i", &num);
+	printf("\nInforme um valor inteiro e maior que 1 para n: ");
+	scanf("%i", &exp);
+	
+	elevado = pow(num, exp);
+	
+	printf("\n%i elevado a %i eh: %i",num,exp,elevado);
+	
+	return 0;
+}
